ca094f0857f3100e433cce4f1e519b5b23f9fbe0
jojorequiem <jonathancourrielpro@gmail.com>
2024-05-10 22:07:23 +0200

    add all directory

--- Puck-Man-Game/src/PuckMan/Game/Program.cs
+++ Puck-Man-Game/src/PuckMan/Game/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Puck_Man_Game.src.PuckMan.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Puck_Man_Game
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainMenuForm());
         }
     }
 }
