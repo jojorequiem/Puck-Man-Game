640e3aa3c4770086e4aaf9d6854db07205a937b7
jojorequiem <jonathancourrielpro@gmail.com>
2024-05-10 22:07:23 +0200

    add all directory

---
+++ Puck-Man-Game/src/PuckMan/UI/MainMenuForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Puck_Man_Game.src.PuckMan.UI
+{
+    partial class MainMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.Name = "MainMenuForm";
+            this.Text = "MainMenuForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
