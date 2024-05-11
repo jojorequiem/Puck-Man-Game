ba7b40c06b50a68e3a5d0cbd53fb0083a4b5581a
jojorequiem <jonathancourrielpro@gmail.com>
2024-05-11 22:13:38 +0200

    feat(class-joueur): Added the beginning of the "Joueur" class

---
+++ Puck-Man-Game/src/PuckMan/Game/Characters/Joueur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puck_Man_Game.src.PuckMan.Game.Characters
+{
+    internal class Joueur
+    {
+        // Propriétés du joueur
+        public string Nom { get; set; }
+        public int PointsDeVie { get; set; }
+        public int Niveau { get; set; }
+
+        // Constructeur
+        public Joueur(string nom, int pointsDeVie, int niveau)
+        {
+            Nom = nom;
+            PointsDeVie = pointsDeVie;
+            Niveau = niveau;
+        }
+
+        // Méthode pour attaquer un adversaire
+        public void getInfo()
+        {
+            Console.WriteLine($"{Nom} {PointsDeVie} {Niveau}");
+
+        }
+
+    }
+}
