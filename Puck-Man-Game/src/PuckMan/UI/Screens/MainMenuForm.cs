8de76f41edcbb4cf7d309b97be9794e2eb6a6bbd
jojorequiem <jonathancourrielpro@gmail.com>
2024-05-11 18:25:12 +0200

    feat(window-linking): Added linking code between Main Menu and Game Parameters window

--- Puck-Man-Game/src/PuckMan/UI/Screens/MainMenuForm.cs
+++ Puck-Man-Game/src/PuckMan/UI/Screens/MainMenuForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Puck_Man_Game.src.PuckMan.UI.Screens;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,19 @@ namespace Puck_Man_Game.src.PuckMan.UI
             InitializeComponent();
         }
 
-   
+        private void btnJouer_Click(object sender, EventArgs e)
+        {
+            MenuParametreJouer menuParametreJouer = new MenuParametreJouer();
+            menuParametreJouer.StartPosition = FormStartPosition.Manual;
+            menuParametreJouer.Location = this.Location;
+            menuParametreJouer.Show();
+            this.Hide(); // Masquer la fenêtre actuelle
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
+
 }
